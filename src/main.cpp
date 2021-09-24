47fc8d26e06858a4c7e2c4ad2298d9dcada8fac0
amilkey1 <analisa.milkey@gmail.com>
2021-09-24 12:40:19 -0400

    initialized vector in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,29 @@
 //  Created by Analisa Milkey on 9/24/21.
 //
 
-#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-    
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    //Create a vector of particles
+    vector<int> vect;
+     
+        vect.push_back(1);
+        vect.push_back(2);
+        vect.push_back(3);
+        vect.push_back(4);
+        vect.push_back(5);
+        vect.push_back(6);
+        vect.push_back(7);
+        vect.push_back(8);
+        vect.push_back(9);
+        vect.push_back(10);
+     
+        for (int x : vect)
+            cout << x << " ";
+     
+        return 0;
 }
