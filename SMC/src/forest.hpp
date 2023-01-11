ebd72059b566d26596250c56bf0dc93cb2cd26fa
amilkey1 <analisa.milkey@gmail.com>
2023-01-11 09:37:21 -0500

    fixed species & gene tree topology priors

--- SMC/src/forest.hpp
+++ SMC/src/forest.hpp
@@ -1050,10 +1050,7 @@ class Forest {
 
         if (_lineages.size()>2) {
             _increments.push_back(make_pair(_last_edge_length, log(rate)-_last_edge_length*rate));
-//            _branch_length_priors.push_back(log(rate)-_last_edge_length*rate);
         }
-
-//        _rand_numbers.push_back(_last_edge_length);
 
         return event;
     }
@@ -1063,14 +1060,11 @@ class Forest {
         double rate = (_speciation_rate+_hybridization_rate)*_lineages.size();
 
         _last_edge_length = rng.gamma(1.0, 1.0/rate);
-//        _branch_length_priors.push_back(log(rate)-_last_edge_length*rate);
-//        _rand_numbers.push_back(_last_edge_length);
 
         for (auto nd:_lineages) {
             nd->_edge_length += _last_edge_length; //add most recently chosen branch length to each species node
         }
         _increments.push_back(make_pair(_last_edge_length, log(rate)-_last_edge_length*rate));
-//        saveDivergenceTimes(_last_edge_length);
     }
 
     inline tuple<string,string, string> Forest::speciesTreeProposal() {
@@ -1103,10 +1097,6 @@ class Forest {
 
         return make_tuple(subtree1->_name, subtree2->_name, new_nd->_name);
     }
-
-//    inline void Forest::saveDivergenceTimes(double edge_len) {
-//        _branch_lengths.push_back(edge_len);
-//    }
 
     inline void Forest::showSpeciesJoined() {
         assert (_index==0);
@@ -2139,161 +2129,16 @@ class Forest {
     }
 
     inline double Forest::calcTopologyPrior() {
-        double sum = 0.0;
-        int numTaxa = 0;
-        if (_index == 0) {
-            numTaxa = _nspecies;
+        double log_joining_prob = 0.0;
+        for (int n=_nspecies; n>1; n--) {
+            log_joining_prob += -log(0.5*n*(n-1));
         }
-        else {
-            numTaxa = _ntaxa;
-        }
-        for (int i = numTaxa; i>1; i--) {
-            sum += log (nChoose2(i));
-        }
-        return 1 - sum;
+        return log_joining_prob;
     }
 
-//inline double Forest::calcTopologyPrior() {
-//       // Compute the probability of the tree topology only
-//       // The probability of a "Markovian" tree topology (both Yule and Coalescent)
-//       // is given in Tanja Stadler's Ph.D. thesis:
-//       //
-//       // https://arxiv.org/pdf/math/0610919.pdf
-//       //
-//       // See example 2.2.6, p. 15, second equation (for P_Y(T)) for an example
-//       // calculation using the ranked, rooted tree in Fig. 1.6. The formula for
-//       // the tree topology (marginalized over all possible rankings) is not
-//       // difficult to calculate:
-//       //
-//       // log[P(T)] = (n-1) log(2) - log(n!) - sum_i log(n_i)
-//       //
-//       // where n_i = number of interior nodes descending from interior node i
-//       // Note that each node counts as a descendant of itself
-//
-//       // Here is the tree in Fig. 1.6:
-//       //
-//       //  a   b   c   d   e   f   g   h   i   j   k    node 10 has  1 descendants
-//       //  |   |   |   |   |   |   |   |   |   |   |    node  9 has  1 descendants
-//       //  |   +-5-+   +-8-+   +-9-+   +10-+   +-7-+    node  8 has  1 descendants
-//       //  |     |       |       |       |       |      node  7 has  1 descendants
-//       //  +--4--+       |       +---6---+       |      node  6 has  3 descendants
-//       //     |          |           |           |      node  5 has  1 descendants
-//       //     +----3-----+           +-----2-----+      node  4 has  2 descendants
-//       //          |                       |            node  3 has  4 descdendants
-//       //          +-----------1-----------+            node  2 has  5 descendants
-//       //                      |                        node  1 has 10 descendants
-//       //
-//       // X = {a,b,c,d,e,f,g,h,i,j,k}
-//       // n = |X| = 11
-//       //
-//       // log[P(T)] = (n-1) log(2) - log(n!) - sum_i log(n_i)
-//       //           = 10*log(2) - lgamma(12) - log(10) - log(5) - log(4) - log(2) - log(1)
-//       //              - log(3) - log(1) - log(1) - log(1) - log(1)
-//       //           = -17.66091
-//       // exp(-17.66091) = 0.213778e-07 accords with answer 0.21e-7 given in the dissertation.
-//       //
-//
-//       // Visiting nodes in preorder sequence...
-//       // node  stack
-//       //   1     1
-//       //   3     1,3
-//       //   4     1,3,4
-//       //   a     ---
-//       //   5     1,3,4,5
-//       //   b     ---
-//       //   c     --- 5 popped (no rsib), 4 popped (first to have rsib)
-//       //   8     1,3,8
-//       //   d     ---
-//       //   e     --- 8 popped (no rsib), 3 popped (first to have rsib)
-//       //   2     1,2
-//       //   6     1,2,6
-//       //   9     1,2,6,9
-//       //   f     ---
-//       //   g     --- 9 popped (first to have rsib)
-//       //  10     1,2,6,10
-//       //   h     ---
-//       //   i     --- 10 popped (no rsib), 6 popped (first to have rsib)
-//       //   7     1,2,7
-//       //   j     ---
-//       //   k     --- 7 popped (no rsib), 2 popped (no rsib), 1 popped (no rsib)
-//       //
-//       //  note that:
-//       //    node 1 appears 10 times,
-//       //    node 2 appears 5 times,
-//       //    node 3 appears 4 times,
-//       //    node 4 appears 2 times,
-//       //    nodes 5, 7, 8, 9, and 10 each appear 1 time, and
-//       //    node 6 appears 3 times
-//
-//       // Should only be called for complete trees (this could be relaxed however)
-//       assert(_lineages.size() == 1);
-//
-//       //cerr << "\n" << makeNewick(5,false) << "\n" << endl;
-//
-//       vector< Node *> node_stack;
-//       map< Node *, unsigned> counts;
-//       unsigned nleaves = 0;
-//       unsigned ninternals = 0;
-//        Node * nd = *(_lineages.begin());
-//       while (nd) {
-//           if (nd->_left_child) {
-//               // internal node
-//               ninternals++;
-//               node_stack.push_back(nd);
-//               for (auto nd : node_stack) {
-//                   counts[nd]++;
-//               }
-//           }
-//           else {
-//               // leaf node
-//               nleaves++;
-//               if (!nd->_right_sib) {
-//                    Node * popped = (node_stack.empty() ? 0 : *(node_stack.rbegin()));
-//                   while (popped && !popped->_right_sib) {
-//                       node_stack.pop_back();
-//                       if (node_stack.empty()) {
-//                           popped = 0;
-//                       }
-//                       else {
-//                           popped = *node_stack.rbegin();
-//                       }
-//                   }
-//                   if (popped && popped->_right_sib) {
-//                       node_stack.pop_back();
-//                   }
-//               }
-//           }   // leaf node
-//           nd = findNextPreorder(nd);
-//       }   // while (nd)...
-//
-//       double log_topology_prob = float(nleaves-1)*log(2.) - lgamma(nleaves+1);
-//
-//       assert(ninternals == counts.size());
-//       for (auto c : counts) {
-//           log_topology_prob -= log(c.second);
-//       }
-//
-//       //cerr << "log_topology_prob = " << log_topology_prob << "\n" << endl;
-//
-//       return log_topology_prob;
-//   }
-
     inline double Forest::findShallowestCoalescence() {
-//        assert(_new_nodes.size() <= _increments.size());
-//        vector<pair<double,double>> searchable_branch_lengths(_increments.end() - _new_nodes.size(), _increments.end());
-//        pair<double, double> smallest_branch = *min_element(_searchable_branch_lengths.begin(), _searchable_branch_lengths.end());
-//        double smallest_branch = *min_element(_searchable_branch_lengths.begin(), _searchable_branch_lengths.end());
         pair<double, double> smallest_branch = *min_element(_searchable_branch_lengths.begin(), _searchable_branch_lengths.end());
-//        showForest();
         return smallest_branch.first;
-
-//        vector<double> searchable_branch_lengths;
-//        for (auto &nd:_lineages) {
-//            searchable_branch_lengths.push_back(nd->_left_child->_edge_length);
-//            searchable_branch_lengths.push_back(nd->_left_child->_right_sib->_edge_length);
-//        }
-//        double smallest_branch = *min_element(searchable_branch_lengths.begin(), searchable_branch_lengths.end());
-//        return smallest_branch;
     }
 
     inline void Forest::revertToShallowest(double smallest_branch){
@@ -2301,12 +2146,8 @@ class Forest {
 
         // save nodes that must be reverted
         vector<Node*> nodes_to_revert;
-//        for (auto &lineage:_lineages) {
-//        showForest();
         for (auto &lineage:_new_nodes) {
             if (lineage->_left_child) {
-//                if (lineage->_left_child->_edge_length != smallest_branch) {
-                    // TODO: does this still work with smallest branch being height not increment?
                 if (getLineageHeight(lineage->_left_child) != smallest_branch) {
                     nodes_to_revert.push_back(lineage);
                     lineage->_name = "unused";
@@ -2317,7 +2158,6 @@ class Forest {
             }
         }
 
-//        showForest();
         assert (nodes_to_revert.size() == _new_nodes.size()-1);
         // TODO: this will only work if there is only one unused node per lineage (which is true for now)
         for (auto &s:_species_partition) {
@@ -2356,7 +2196,6 @@ class Forest {
         }
 
         // reset branches to smallest coalescence
-//        showForest();
         for (auto &lineage:_lineages) {
             if (lineage->_edge_length != 0.0) {
                 if (!lineage->_left_child) {
