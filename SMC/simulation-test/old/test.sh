82c30c348dfa502eb741618837dacd1dc7e1141e
aam21005 <analisa.milkey@gmail.com>
2024-02-06 15:54:03 -0500

    updated deploy script for multiple simulations at a time

---
+++ SMC/simulation-test/old/test.sh
@@ -0,0 +1,29 @@
+cp rep1/sim/sim.nex ../smc
+cp rep1/sim/sim.nex ../beast
+
+cp rep2/sim/sim.nex ../smc
+cp rep2/sim/sim.nex ../beast
+
+cp rep3/sim/sim.nex ../smc
+cp rep3/sim/sim.nex ../beast
+
+cp rep4/sim/sim.nex ../smc
+cp rep4/sim/sim.nex ../beast
+
+cp rep5/sim/sim.nex ../smc
+cp rep5/sim/sim.nex ../beast
+
+cp rep6/sim/sim.nex ../smc
+cp rep6/sim/sim.nex ../beast
+
+cp rep7/sim/sim.nex ../smc
+cp rep7/sim/sim.nex ../beast
+
+cp rep8/sim/sim.nex ../smc
+cp rep8/sim/sim.nex ../beast
+
+cp rep9/sim/sim.nex ../smc
+cp rep9/sim/sim.nex ../beast
+
+cp rep10/sim/sim.nex ../smc
+cp rep10/sim/sim.nex ../beast
