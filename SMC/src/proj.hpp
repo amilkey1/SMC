3fd530248e31c8584cfc6a119cef966d14afc493
amilkey1 <analisa.milkey@gmail.com>
2025-06-09 07:28:01 -0400

    added sliding window mcmc to command line options

--- SMC/src/proj.hpp
+++ SMC/src/proj.hpp
@@ -876,6 +876,7 @@ namespace proj {
         ("ngroups", boost::program_options::value(&G::_ngroups)->default_value(1), "number of populations")
         ("upgma", boost::program_options::value(&G::_upgma)->default_value(true), "set to false to not use UPGMA completion")
         ("mcmc", boost::program_options::value(&G::_mcmc)->default_value(false), "use mcmc moves in analysis")
+        ("sliding_window", boost::program_options::value(&G::_sliding_window)->default_value(0.05), "size of sliding window to use in mcmc analysis")
 #if defined(SPECIES_IN_CONF)
         ("species", boost::program_options::value(&species_definitions), "a string defining a species, e.g. 'A:x,y,z' says that taxa x, y, and z are in species A")
 #endif
@@ -3224,8 +3225,6 @@ namespace proj {
                 
                 Particle p;
                 initializeParticle(p); // initialize one particle and copy to all other particles
-                                
-                G::_sliding_window = 0.05; // TODO: set in command line
                 
                 // reset marginal likelihood
                 _log_marginal_likelihood = 0.0;
